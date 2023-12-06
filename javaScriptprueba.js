05e1843e60be0dabf3a3a5b4d5932b94adbbbb3f
rgstitt <racedgiraldo009@gmail.com>
2023-12-05 20:23:05 -0500

    tarea1

---
+++ javaScriptprueba.js
@@ -0,0 +1,59 @@
+// console.log (1+1)
+// console.log ("hola" + " mundo");
+// console.log (1 + "1")   
+// // console.log (1+1+"1")
+// console.log ( 1 + 5 * 20 );
+// var A = 10
+// var B = 5
+// var C = 5
+// var d = A = B
+// console.log  ("el valor de D es" + d)
+// operadores de comparacion
+
+// var var1 = 1
+// var var2 = "1"
+// console.log ( "usando == " , var1 == var2)
+// console.log ("usando" , var1 === var2)
+
+// var ganoElPartido = true
+// var pasaDeRonda = true
+// console.log (ganoElPartido && pasaDeRonda)
+// var ganoLaFinal = false
+// var hizoGol = true
+// console.log (hizoGol || ganoLaFinal )
+// var prueba = "quiero saber uantos caracteres tiene esta frase " 
+// console.log (prueba.length)
+// console.log (prueba.toUpperCase())
+// function nombreMascotas(){console.log ("manolo y candy son",  1 + 1 +" perros")
+// return 45 + 7 }
+// console.log (nombreMascotas());
+// function suma (a, b){return a + b
+// }
+// console.log (suma(1,2));
+// function returnTrue () { return true}
+
+// console.log (returnTrue());
+// var pruebaVariable = true
+// (pruebaVariable === true) {
+// console.log ("esto es true");
+// } else { console.log (esto es false);}
+
+// var saludo =  "hola "
+// if (saludo === "hola"){
+// console.log ("esta saludando")}
+// else if (saludo === "chao"){console.log ("esta despidiendo"
+// )}
+// else { console.log ("no sabemos que esta haciendo")}
+// if (1 === 0) {
+//     console.log ( " esto es true " );
+// } else {console.log ("esto es false");
+// }
+// if (true) {console.log ("esto es  true")}
+// else {console.log("esto es false")}
+function saludos (idioma){  
+    if (idioma === "es") {return "hola"}
+
+else if  (idioma === "en") {return "hello" }
+else { return "no sabemos que idioma es"}}
+
+console.log( saludos(""))
